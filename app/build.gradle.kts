9db713829f847ab55763c4776797f98290fb8126
Marius-Ma <my429542819@gmail.com>
2025-05-22 21:30:45 +1000

    back up all of home

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -86,4 +86,11 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+
+    // retrofit
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
+
+    implementation("com.google.android.gms:play-services-location:21.0.1")
 }
